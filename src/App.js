0f9aea41de98ebc1112d46ca646b5348f5b98df4
John Bogil <johncbogil@gmail.com>
2023-07-01 21:04:58 -0400

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import xr from './xr.png';
 import './App.css';
+import { Box, Container, Paper, Typography } from '@mui/material';
+
+const serviceList = [
+  "422.61 ppm", "The ultimate, hidden truth of the world is that it is something that we make, and could just as easily make differently.<br><br>-David Graeber",
+  "image"
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {serviceList.map((service) => (
+        <Paper sx={{ m: 10 }} elevation={3}>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            {service === "image" ? (
+              <img src={xr} alt="xr" />
+            ) : (
+              <Typography
+                sx={{ p: 5 }}
+                variant="h2"
+                dangerouslySetInnerHTML={{ __html: service }}
+              />
+            )}
+          </Box>
+        </Paper>
+      ))}
+    </Container>
   );
 }
 
