8cfebb6739e123d83e744b3c42cff207c07b41e0
John Bogil <johncbogil@gmail.com>
2023-07-03 21:34:13 -0400

    layout changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Tile from './Components/Tile';
 
 const serviceList = [
   "422.61 ppm",
+  "image",
   "The ultimate, hidden truth of the world is that it is something that we make, and could just as easily make differently.<br><br>-David Graeber",
   "Lorem ipsum dolor sit amet",
   "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-  "image",
   "12345 xyz",
   "Sed ut perspiciatis unde omnis",
   "Neque porro quisquam est"
@@ -16,16 +16,11 @@ function App() {
   return (
     <Container>
       <Grid container spacing={3}>
-        <Grid item>
-          {serviceList.slice(0, 4).map((content, index) => (
-            <Tile key={index} content={content} index={index}/>
-          ))}
-        </Grid>
-        <Grid item>
-          {serviceList.slice(4).map((content, index) => (
-            <Tile key={index} content={content} index={index}/>
-          ))}
-        </Grid>
+        {serviceList.map((content, index) => (
+          <Grid item xs={12} sm={6} md={3} key={index}>
+            <Tile content={content} index={index}/>
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
